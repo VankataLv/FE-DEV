04ac8f53a3474697631523fda0c477b6332c3195
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-03-23 08:13:13 +0200

    23.03.2024

--- 02. JS/11. DOM Intro/02.Collect List Items/CollectListItems.js
+++ 02. JS/11. DOM Intro/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    const itemsElement = document.getElementById('items');
+    const textAreaElement = document.getElementById('result');
+
+    textAreaElement.value = itemsElement.textContent;
+
 }
--- 02. JS/11. DOM Intro/03.Sum Numbers/SumNumbers.js
+++ 02. JS/11. DOM Intro/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,19 @@
+// function calc() {
+//     const firstInputElement = document.getElementById('num1');
+//     const seconfInputElement = document.querySelector('#num2');
+//     const sumInputElement = document.getElementById('sum');
+
+//     const firstNum = Number(firstInputElement.value);
+//     const secondNum = Number(seconfInputElement.value);
+//     const sum = firstNum + secondNum;
+
+//     sumInputElement.value = sum
+
+// }
+
 function calc() {
-    // TODO: sum = num1 + num2
+    const firstNum = Number(document.getElementById('num1').value);
+    const secondNum = Number(document.getElementById('num2').value);
+
+    document.getElementById("sum").value = firstNum + secondNum
 }
