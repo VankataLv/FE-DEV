92232da543dca10bc21bf3fa76ec59b914757fbc
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-03-23 17:22:27 +0200

    23.03.2024

--- 02. JS/12. Exercise DOM Intro/03. Accordion/accordion.js
+++ 02. JS/12. Exercise DOM Intro/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+    const toggleButtonEl = document.querySelector('.head span.button');
+    const elToReveal = document.getElementById('extra');
+
+    const currentButtonText = toggleButtonEl.textContent;
+
+    if (currentButtonText == "More") {
+        toggleButtonEl.textContent = "Less";
+        elToReveal.style.display = 'block';
+    }
+    else {
+        toggleButtonEl.textContent = "More";
+        elToReveal.style.display = 'none';
+        elToReveal.style.background = "green"
+    }
 }
+
