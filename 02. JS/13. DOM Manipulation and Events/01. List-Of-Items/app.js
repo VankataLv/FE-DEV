6d786eb8f5a7d1c649f633c806a0093acda69926
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-03-25 21:04:41 +0200

    25.03.2024

--- 02. JS/13. DOM Manipulation and Events/01. List-Of-Items/app.js
+++ 02. JS/13. DOM Manipulation and Events/01. List-Of-Items/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputElement = document.getElementById('newItemText');
+    const itemListElement = document.getElementById('items');
+
+    //Create new item
+    const newItemElement = document.createElement('li')
+    // Add text content
+    newItemElement.textContent = inputElement.value;
+    // Append to DOM
+    itemListElement.appendChild(newItemElement)
+    // Clear input Element
+    inputElement.value = "";
 }
--- 02. JS/13. DOM Manipulation and Events/02. Delete-From-Table/app.js
+++ 02. JS/13. DOM Manipulation and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,37 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputFieldElement = document.querySelector('input[name=email]');
+    const resultFieldElement = document.getElementById('result');
+    const tableSecondColumn = document.querySelectorAll('table#customers tbody tr td:nth-child(2')
+    let successfylDeletion = false;
+
+    for (td of tableSecondColumn) {
+        if (td.textContent == inputFieldElement.value) {
+            let rowToBeDeleted = td.parentNode;
+            rowToBeDeleted.parentNode.removeChild(rowToBeDeleted);
+            successfylDeletion = true
+        }
+    }
+    if (successfylDeletion) {
+        resultFieldElement.textContent = "Deleted.";
+    }
+    else {
+        resultFieldElement.textContent = "Not Found.";
+    }
+
+    inputFieldElement.value = "";
 }
+
+// function deleteByEmail() {
+//     let email = document.getElementsByName("email")[0].value;
+//     let secondColumn = document.querySelectorAll("#customers tr td:nth-child(2)");
+//     for (let td of secondColumn)
+//     if (td.textContent == email) {
+//     let row = td.parentNode;
+//     row.parentNode.removeChild(row);
+//     document.getElementById('result').
+//     textContent = "Deleted.";
+//     return;
+//     }
+//     document.getElementById('result').textContent =
+//    "Not found.";
+//    }
